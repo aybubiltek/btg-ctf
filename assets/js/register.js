da112f40c13f60ba4724315dfc43b6d39e3c5f9f
Oğuzhan Korkmaz <oguzhannkorkmaz@gmail.com>
2021-06-19 19:03:57 +0300

    edit vol bilmem kaç

--- assets/js/register.js
+++ assets/js/register.js
@@ -4,22 +4,31 @@
 //   $("form#" + form_name).removeClass("hid");
 // }
 
-
 $(".checkbox-s").click(function () {
-    $(".checkbox-s i").toggleClass("show");
-    $("#accept").click();
-})
+  $(".checkbox-s i").toggleClass("show");
+  $("#accept").click();
+  if ($("input#accept").is(":checked")) {
+    $(".team-btn-area").removeClass("hidden");
+  }
+  else{
+    $(".team-btn-area").addClass("hidden");
+    $("#newTeam-form").addClass("hidden");
+    $("#addTeam-form").addClass("hidden");
+    $("#addTeam").removeClass("hidden");
+    $("#newTeam").removeClass("hidden");
+  }
+});
 
-$("#newTeam").click(function (){
-    $(this).addClass("hidden")
-    $("#addTeam-form").addClass("hidden")
-    $("#newTeam-form").removeClass("hidden")
-    $("#addTeam").removeClass("hidden")
-})
+$("#newTeam").click(function () {
+  $(this).addClass("hidden");
+  $("#addTeam-form").addClass("hidden");
+  $("#newTeam-form").removeClass("hidden");
+  $("#addTeam").removeClass("hidden");
+});
 
-$("#addTeam").click(function (){
-    $(this).addClass("hidden")
-    $("#newTeam-form").addClass("hidden")
-    $("#addTeam-form").removeClass("hidden")
-    $("#newTeam").removeClass("hidden")
-})
+$("#addTeam").click(function () {
+  $(this).addClass("hidden");
+  $("#newTeam-form").addClass("hidden");
+  $("#addTeam-form").removeClass("hidden");
+  $("#newTeam").removeClass("hidden");
+});
