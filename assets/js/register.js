d11dc9e081f182accf38d2d806508d77ee834493
Oğuzhan Korkmaz <oguzhannkorkmaz@gmail.com>
2021-06-19 18:52:24 +0300

    edit register pae

--- assets/js/register.js
+++ assets/js/register.js
@@ -1,10 +1,25 @@
-function formChange(form_name) {
-  $("form#information-form").addClass("hid");
-  $("form#" + form_name).removeClass("hid");
-}
+// function formChange(form_name) {
+// //   $("form#information-form").addClass("hid");
+//   $("form").addClass("hid");
+//   $("form#" + form_name).removeClass("hid");
+// }
 
 
 $(".checkbox-s").click(function () {
     $(".checkbox-s i").toggleClass("show");
     $("#accept").click();
+})
+
+$("#newTeam").click(function (){
+    $(this).addClass("hidden")
+    $("#addTeam-form").addClass("hidden")
+    $("#newTeam-form").removeClass("hidden")
+    $("#addTeam").removeClass("hidden")
+})
+
+$("#addTeam").click(function (){
+    $(this).addClass("hidden")
+    $("#newTeam-form").addClass("hidden")
+    $("#addTeam-form").removeClass("hidden")
+    $("#newTeam").removeClass("hidden")
 })
